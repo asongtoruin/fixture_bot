e1817bd22d0b5afc8a3ffd2cc6117f7b88fe0e72
Adam <adam.ruszkowski@gmail.com>
2020-09-20 20:10:30 +0100

    Add some basic replies

--- bot.py
+++ bot.py
@@ -4,10 +4,29 @@ from datetime import datetime
 from discord.ext import commands, tasks
 
 from params import TARGET_CHANNEL_ID, TOKEN
-from fixtures import get_active_fixtures
+from fixtures import get_active_fixtures, Fixture
 
 
-bot = commands.Bot('!')
+bot = commands.Bot(command_prefix=commands.when_mentioned_or('!'))
+
+
+help_text = f'''I post fixtures for predefined teams at 8am each day.
+
+__**Teams**__
+Fixtures are shown alphabetically for the teams I'm tracking. Those teams are:
+{', '.join(Fixture.BADGE_LOOKUPS.values())}
+
+__**Form**__
+Form is shown by default for the past 5 matches for a team, from most to least 
+recent. Form is shown across all competitions, and does not include penalties 
+(so, if a match goes to penalties it's recorded as a draw).
+
+The following icons are used:
+:blue_circle: - win
+:red_circle: - loss
+:yellow_circle: - draw
+:black_circle: - match postponed / cancelled / other.
+'''
 
 POST_TIME = datetime.strptime('08:00', '%H:%M')
 
@@ -38,19 +57,20 @@ async def time_wait():
     await asyncio.sleep(wait_time)
     print('Ready for scheduled posting!')
 
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
 
-# @bot.event
-# async def on_message(message):
-#     if message.author == bot.user:
-#         return
-
-#     if message.content.startswith('$hello'):
-#         await message.channel.send(f'Hello {message.author.mention}')
-    
-#     if message.content.startswith('$teams'):
-#         await message.channel.send(
-#             f'{message.author.mention} - available teams are'
-#         )
+    start_text = f'Hello {message.author.mention}!'
+    if 'help' in message.content:
+        await message.channel.send(
+            start_text + ' Here is some info on how I work:\n' + help_text
+        )
+    else:
+        await message.channel.send(
+            start_text + ' I don\'t know how to respond to that :robot:'
+        )
 
 post_fixtures.start()
 bot.run(TOKEN)
