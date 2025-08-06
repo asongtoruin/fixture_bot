9f0ecdc5e71cb83bd396f0bbf731e20baf85ad86
Adam Ruszkowski <adam.ruszkowski@gmail.com>
2025-08-06 17:44:39 +0100

    Add in ability to use Fixtures endpoint
    Damn this one is fussy. Precise usage will be needed!

--- src/output_models.py
+++ src/output_models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from os import PathLike
 
 from pydantic import BaseModel, HttpUrl
@@ -28,30 +29,90 @@ class BaseResponse(FileIOModel):
     paging: Pagination
 
 
-class Team(FileIOModel):
+class BasicTeam(FileIOModel):
     id: int
     name: str
+    logo: HttpUrl
+
+    def __eq__(self, other):
+        if not isinstance(other, BasicTeam):
+            return False
+    
+        return self.id == other.id
+
+
+class TeamResult(BasicTeam):
+    winner: bool | None
+
+
+class TeamInfo(BasicTeam):
     code: str
     country: str
     founded: int
     national: bool
-    logo: HttpUrl
 
 
-class Venue(FileIOModel):
-    id: int
-    name: str
+class MatchStatus(FileIOModel):
+    long: str
+    short: str
+    elapsed: int | None
+
+
+class SimpleVenue(FileIOModel):
+    id: int | None
+    name: str | None
+    city: str | None
+
+
+class VenueInfo(SimpleVenue):
     address: str
-    city: str
     capacity: int
     surface: str
     image: HttpUrl
 
 
 class IndividualTeamResponse(FileIOModel):
-    team: Team
-    venue: Venue
+    team: TeamInfo
+    venue: VenueInfo
+
+
+class League(FileIOModel):
+    id: int
+    name: str
+    country: str
+    logo: HttpUrl
+    flag: HttpUrl | None
+    season: int
+    round: str
+
+
+class Fixture(FileIOModel):
+    id: int
+    referee: str | None = None
+    timezone: str
+    date: datetime
+    timestamp: int
+    periods: dict
+    venue: SimpleVenue
+    status: MatchStatus
+
+
+class FixtureTeams(FileIOModel):
+    home: TeamResult
+    away: TeamResult
+
+
+class FixtureInfo(FileIOModel):
+    fixture: Fixture
+    league: League
+    teams: FixtureTeams
+    goals: dict
+    score: dict
 
 
 class TeamsResponse(BaseResponse):
     response: list[IndividualTeamResponse]
+
+
+class FixturesResponse(BaseResponse):
+    response: list[FixtureInfo]
